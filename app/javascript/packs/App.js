f807f41bf28333b9f40275e3a69721d136357bce
Josh Teperman <Josh.teperman@weployapp.com>
2019-11-08 08:46:32 +1100

    wip

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import styled from 'styled-components';
+import CodeMirror from './components/CodeMirror';
 
 const App = () => {
   const options = {
     lineNumbers: true,
     tabSize: 2,
+    setSize: 100,
   }
   return (
-    <div>
-      <p>Index (App) Component</p>
-    </div>
+    <CodeMirror options={options} language={'javascript'} ></CodeMirror>
   )
 }
 
--- app/javascript/packs/components/Codemirror.js
+++ app/javascript/packs/components/Codemirror.js
@@ -1,22 +1,40 @@
-import React, { useEffect, useRef } from 'react';
-import CodeMirror from 'codemirror';
+import React, { useEffect, useState, useRef } from 'react';
+import Codemirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
 
-const Codemirror = ({ options, language, }) => {
-  const inputElement = useRef();
-
+const CodeMirror = ({ options, language }) => {
   require(`codemirror/mode/${language}/${language}`);
+  const inputElement = useRef();
+  const [codeValue, setCodeValue] = useState('initial value');
 
   useEffect(() => {
-    CodeMirror.fromTextArea(inputElement.current, options)
+    const myCodeMirror = Codemirror.fromTextArea(inputElement.current, options)
+    setCodeValue(myCodeMirror.getValue())
   }, [options]);
 
+  useEffect(() => {
+    console.log(codeValue);
+  }, [codeValue])
+
+  const handleSubmit = (event, textArea) => {
+    event.preventDefault()
+    console.log(textArea.current.value);
+  }
+
+  const handleChange = () => {
+    console.log('changed')
+    setCodeValue('HELLO')
+  }
+
   return (
-    <textarea ref={inputElement} defaultValue="New note ..." />
+    <>
+    <form action="#" >
+      <label>CodeMirror Form</label>
+      <textarea onChange={() => handleChange} ref={inputElement} defaultValue="Write some code ..." />
+      <input type="submit" value="Submit" onClick={(event) => handleSubmit(event, inputElement)} />
+    </form>
+    </>
   );
 };
 
-export default Codemirror;
-
-// import CodeMirror from './components/Codemirror';
-// <CodeMirror options={options} language={'javascript'} />
+export default CodeMirror;
