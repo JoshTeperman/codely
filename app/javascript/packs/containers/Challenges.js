cacacb1c11d2ae6ff2c5a80d764c7f304736e68a
Josh Teperman <joshteperman@gmail.com>
2019-11-11 08:01:41 +1100

    Set useEffect for GET_CHALLENGES response. TODO later: set pollingInterval

--- app/javascript/packs/containers/Challenges.js
+++ app/javascript/packs/containers/Challenges.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/react-hooks';
 import { GET_CHALLENGES } from './graphql/queries/getChallenges';
 
 import Challenge from '../components/Challenge';
 
 const Challenges = () => {
+  const [challenges, setChallenges] = useState(null)
   const { loading, error, data } = useQuery(GET_CHALLENGES)
-
-  if (loading) return <h3>...Loading</h3>
-  if (error) return <p>`Error: ${error}`</p>
-
-  const { challenges } = data
-
-  return challenges.map(challenge => <Challenge key={challenge.id} challenge={challenge} />)
+  
+  useEffect(() => {
+    if (!loading && data) {
+      setChallenges(data.challenges)
+    }
+  }, [loading, data, error])
+  
+  return (
+    <>
+      {loading && <h3>...Loading</h3>}
+      {challenges && challenges.map(challenge => <Challenge key={challenge.id} challenge={challenge} />)}
+    </>
+  )
 }
 
 export default Challenges;
