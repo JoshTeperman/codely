6a63a3ecf09eab370cc77d54aa54e5a2ffa6e8ea
Josh Teperman <joshteperman@gmail.com>
2019-11-10 21:50:36 +1100

    Imported CodeMirror component from other branch
    There were too many conflicts to merge the entire branch so just copied the
    component over.
    
    Renamed the exported component `Editor`, which returns the actual CodeMirror component.

---
+++ app/javascript/packs/containers/Editor.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState, useRef } from 'react';
+import Codemirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+
+const CodeMirror = ({ options, language }) => {
+  require(`codemirror/mode/${language}/${language}`);
+  const inputElement = useRef();
+  const editor = useRef();
+  const [code, setCode] = useState('initial value');
+
+  useEffect(() => {
+    editor.current = Codemirror.fromTextArea(inputElement.current, options)
+    setCode(editor.current.getValue())
+  }, [options]);
+
+  useEffect(() => {
+    console.log('code value changed', { code });
+  }, [code])
+
+  const handleSubmit = (event, editor) => {
+    event.preventDefault()
+    const currentCode = editor.current.getValue()
+    if (code !== currentCode) { setCode(currentCode) }
+  }
+
+  return (
+    <>
+    <form>
+      <label>CodeMirror Form</label>
+      <textarea ref={inputElement} defaultValue="Write some code ..." />
+      <button onClick={(event) => handleSubmit(event, editor)}>Submit</button>
+    </form>
+    </>
+  );
+};
+
+const Editor = () => {
+  const options = {
+    lineNumbers: true,
+    tabSize: 2,
+  }
+
+  return(
+    <CodeMirror options={options} language={'javascript'} ></CodeMirror>
+  )
+}
+
+export default Editor;
