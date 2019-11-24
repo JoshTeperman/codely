f8bd083b82f054b37b6d82128053fa6f41f556b4
Josh Teperman <Josh.teperman@weployapp.com>
2019-11-25 09:21:07 +1100

    common InputField component

---
+++ app/javascript/packs/components/common/InputField.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import { Input as MUIInput } from '@material-ui/core';
+
+const StyledInput = styled(MUIInput)`
+`;
+
+const InputField = ({
+  placeholder,
+  required,
+  multiline,
+  defaultValue,
+  fullWidth,
+  autoFocus,
+}) => (
+  <StyledInput
+    placeholder={placeholder}
+    required={required}
+    multiline={multiline}
+    defaultValue={defaultValue}
+    fullWidth={fullWidth}
+    autoFocus={autoFocus}
+  />
+);
+
+InputField.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+  required:  PropTypes.bool,
+  multiline: PropTypes.bool,
+  defaultValue: PropTypes.string,
+  fullWidth: PropTypes.bool,
+  autoFocus: PropTypes.bool,
+}
+
+InputField.defaultProps = {
+  required: false,
+  multiline: false,
+  defaultValue: "",
+  fullWidth: true,
+  autoFocus: false,
+}
+
+export default InputField;
