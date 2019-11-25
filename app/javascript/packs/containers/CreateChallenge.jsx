2c0bdeb5d6b3bbbe46fdaca5b78602cb44e79370
Josh Teperman <Josh.teperman@weployapp.com>
2019-11-26 07:44:43 +1100

    get CreateChallenge form working

--- app/javascript/packs/containers/CreateChallenge.jsx
+++ app/javascript/packs/containers/CreateChallenge.jsx
@@ -5,16 +5,17 @@ import Form from '../components/common/Form';
 import InputField from '../components/common/InputField';
 import Button from '../components/common/Button';
 
-const handleSubmit = (event) => {
-  event.preventDefault()
-  console.log({name, description, language});
-}
 
 const CreateChallenge = (props) => {
 
   const [name, setName] = useState("");
   const [description, setDescription] = useState('');
   const [language, setLanguage] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log({name, description, language});
+  };
 
   return (
     <Form>
@@ -37,7 +38,7 @@ const CreateChallenge = (props) => {
         onChange={e => setLanguage(e.target.value)}
       />
       <Button
-        onClick={(event) => handleSubmit(event)}
+        onClick={handleSubmit}
       >
         Create
       </Button>
